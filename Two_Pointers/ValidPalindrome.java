a3303effd4437393bce5090d54e6e9a4011b3d3c
Parameshwaran G <parameshwarangdev@gmail.com>
2026-07-31 20:46:44 +0530

    Two Pointers : Valid Palindrome

---
+++ Two_Pointers/ValidPalindrome.java
@@ -0,0 +1,22 @@
+public class ValidPalindrome{
+    public static void main(String[] args){
+        String str = "football";
+        int left =  0;
+        int right = str.length()-1;
+        boolean flag = true;
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                flag=false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if(flag){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
