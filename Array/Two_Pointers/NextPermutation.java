785e44231a0b5ce0c1e14fa37b893c48b08e5b03
shyamnath-sankar <shyamnathsankar.s@gmail.com>
2026-08-08 10:33:41 +0530

    Two Pointers: Next Permutation

---
+++ Array/Two_Pointers/NextPermutation.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+class NextPermutation{
+    public static void main(String[] args){
+        int nums[] = {1,2,3};
+        int i = nums.length-2;
+        while(i>=0 && nums[i+1]<=nums[i]){
+            i--;
+        }
+        if(i>=0){
+            int j = nums.length-1;
+            while(nums[j]<=nums[i]){
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        i += 1;
+        int j = nums.length-1;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
